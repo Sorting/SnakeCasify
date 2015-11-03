44f83087109bb44a1e84499bbcd9d2536c0f044c
Jimmy Sorting <jimmy@sorting.se>
2015-11-03 23:36:20 +0100

    Remove irrelevant methods and files from the ASP.NET 5 sample app

--- samples/SampleApp/Controllers/ValuesController.cs
+++ samples/SampleApp/Controllers/ValuesController.cs
@@ -28,23 +28,5 @@ namespace SampleApp.Controllers
         {
             return "value: " + postName;
         }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
